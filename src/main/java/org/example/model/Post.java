9b33e3c88cb49b89938b70716620e02ad49d378c
Elena <bond.elena.15.05@gmail.com>
2024-10-16 13:53:09 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/org/example/model/Post.java
@@ -0,0 +1,39 @@
+package org.example.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Post post)) {
+            return false;
+        }
+        return Objects.equals(link, post.link) && Objects.equals(description, post.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(link, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created
+                + '}';
+    }
+}
