bd1fba539db146cba77827f186b78fb4a7258760
Elena <bond.elena.15.05@gmail.com>
2024-10-17 10:05:05 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/org/example/jsoup/HabrCareerParse.java
+++ src/main/java/org/example/jsoup/HabrCareerParse.java
@@ -5,6 +5,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -13,10 +15,12 @@ public class HabrCareerParse {
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
     public static final int PAGES = 5;
+
+    private static final Logger LOG = LoggerFactory.getLogger(HabrCareerParse.class.getName());
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     public static void main(String[] args) throws IOException {
-        for (int i = 0; i < PAGES; i++) {
+        for (int i = 1; i <= PAGES; i++) {
             String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, PAGES, SUFFIX);
             Connection connection = Jsoup.connect(fullLink);
             Document document = connection.get();
@@ -31,5 +35,20 @@ public class HabrCareerParse {
                 System.out.printf("%s %s %s%n", datetimeElement.attr("datetime"), vacancyName, link);
             });
         }
+    }
+
+    private String retrieveDescription(String link) {
+        Connection connection = Jsoup.connect(link);
+        String text = null;
+        try {
+            Document document = connection.get();
+            Element row = document.select(".vacancy-description__text").first();
+            if (row != null) {
+                text = row.text();
+            }
+        } catch (IOException e) {
+            LOG.error("Something is wrong with link {}", link, e);
+        }
+        return text;
     }
 }
