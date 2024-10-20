b5ad2d854045d6dfa281c1f4c85d2528528d6e17
Elena <bond.elena.15.05@gmail.com>
2024-10-20 11:47:16 +0300

    5. PsqlStore [#285209]

--- src/main/java/org/example/repo/Store.java
+++ src/main/java/org/example/repo/Store.java
@@ -4,7 +4,7 @@ import org.example.model.Post;
 
 import java.util.List;
 
-public interface Store {
+public interface Store extends AutoCloseable {
     void save(Post post);
 
     List<Post> getAll();
